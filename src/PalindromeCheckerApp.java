2b60fdc61a02a94fb907fedbf4bee595c1427f89
PranavikaPabbineedi <pp2737@srmist.edu.in>
2026-03-02 14:45:42 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,2 +1,25 @@
 public class PalindromeCheckerApp {
+    public class PalindromeChecker {
+
+        public static void main(String[] args) {
+
+            // Hardcoded string (String Literal)
+            String word = "madam";
+
+            // Reverse the string
+            String reversed = "";
+
+            for (int i = word.length() - 1; i >= 0; i--) {
+                reversed = reversed + word.charAt(i);
+            }
+
+            // Check if original and reversed strings are equal
+            if (word.equals(reversed)) {
+                System.out.println(word + " is a Palindrome.");
+            } else {
+                System.out.println(word + " is NOT a Palindrome.");
+            }
+
+        }
+    }
 }
